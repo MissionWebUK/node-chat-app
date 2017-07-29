0624a457b6ebb66de903b369270ca7539d010dfb
Martin Sanders <martin26sanders@btinternet.com>
2017-07-29 22:02:04 +0100

    Add Mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -16,11 +16,17 @@ socket.on('newMessage', function (message) {
 
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
+  var template = jQuery('#message-template').html();
 
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
+  var html = Mustache.render(template, {
 
-  jQuery('#messages').append(li);
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+
+  });
+
+  jQuery('#messages').append(html);
 
 });
 
@@ -28,14 +34,24 @@ socket.on('newLocationMessage', function (message) {
 
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  var template = jQuery('#location-message-template').html();
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
+  var html = Mustache.render(template, {
 
-  jQuery('#messages').append(li);
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+
+  });
+
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+
+  jQuery('#messages').append(html);
 
 });
 
